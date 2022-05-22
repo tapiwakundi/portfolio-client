79baf7bfdb6799699f25671a7ce3da92e11d56b8
Tapiwa Kundishora <tapiwakundi@gmail.com>
2022-05-22 10:57:24 -0600

    update logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,10 @@
 import styles from './index.module.css'
+const LOGO = require('../../assets/logo.svg').default
 
 export const Logo = () => {
     return (
-        <a href="/">
-            <img src="https://www.freeiconspng.com/uploads/spotify-icon-18.png" className={styles.logo} alt="logo" />
+        <a href="/" className={styles.logo}>
+            <img src={LOGO} alt="logo" className={styles.image} />
         </a>
     )
 }
